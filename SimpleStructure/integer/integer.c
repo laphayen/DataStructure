eed06f8732cf07286c1da812665dbcd356f77945
Baek Gi Chan <11618nathan@gmail.com>
2017-09-04 13:57:54 +0900

    Add files via upload

---
+++ SimpleStructure/integer/integer.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+
+void main()
+{
+	// 정수형
+
+	char ch = 'a';				// 1바이트
+	
+	short sh = 11618;			// 2바이트
+	
+	int num = 11618;			// 4바이트
+	
+	long lo = 11618;			// 4바이트
+	
+	long long lolo = 11618;		// 8바이트
+}

c4f11f3eebf0de9fc61085bab118eaae7d522e19
Baek Gi Chan <11618nathan@gmail.com>
2017-09-05 23:01:33 +0900

    Add files via upload

---
+++ SimpleStructure/realNumber/realNumber.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+
+void main()
+{
+	// 실수형
+	float fl = 1.1618;			// 4바이트
+	double db = 1.1618;			// 8바이트
+	long double lod = 1.1618;	// 8바이트
+
+								// 실수형
+								// float - 4바이트 - 1.2E-38~3.4E38
+								// double - 8바이트 - 2.2E-308~1.8E308
+								// long double - 8바이트 이상 - double 이상의 표현 범위
+}
+}

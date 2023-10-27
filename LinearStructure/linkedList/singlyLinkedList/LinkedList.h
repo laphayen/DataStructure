ba4b39c9efede35e34db56685b897b08ccddce6d
Baek Gi Chan <laphayen@gmail.com>
2023-10-27 15:10:33 +0900

    Upload SinglyLinkedList

---
+++ LinearStructure/linkedList/singlyLinkedList/LinkedList.h
@@ -0,0 +1,21 @@
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct tagNode
+{
+	int Data;
+	struct tagNode* NextNode;
+} Node;
+
+Node* SSL_CreateNode(int NewData);
+void SSL_DestroyNode(Node* Node);
+void SSL_AppendNode(Node** Head, Node* NewNode);
+void SLL_InsertNode(Node* Current, Node* NewNode);
+void SSL_RemoveNode(Node** Head, Node* Remove);
+Node* SLL_GetNode(Node* Head, int Location);
+int SLL_GetNodeCount(Node* Head);
+
+#endif // LINKEDLIST_H
---
+++ LinearStructure/linkedList/singlyLinkedList/Test_LinkedList.h
@@ -0,0 +1,6 @@
+#include "LinkedList.h"
+
+int main(void)
+{
+	
+}

7fc6720a19014cf11777306278167e0be0dc52de
Baek Gi Chan <laphayen@gmail.com>
2023-10-30 02:16:07 +0900

    Update DoublyLinkedList.c

---
+++ LinearStructure/LinkedList/DoublyLinkedList/DoublyLinkedList.c
@@ -0,0 +1,108 @@
+#include "DoublyLinkedList.h"
+
+// DLL 노드 생성
+Node* DLL_CreateNode(int NewData)
+{
+    Node* NewNode = (Node*)malloc(sizeof(Node));
+
+    NewNode->Data = NewData;
+
+    NewNode->PrevNode = NULL;
+    NewNode->NextNode = NULL;
+
+    return NewNode;
+}
+
+// DLL 노드 소멸
+void DLL_DestroyNode(Node* Node)
+{
+    free(Node);
+}
+
+// DLL 노드 추가
+void DLL_AppendNode(Node** Head, Node* NewNode)
+{
+    if ((*Head) == NULL)
+    {
+        *Head = NewNode;
+    }
+    else
+    {
+        Node* Tail = (*Head);
+        while (Tail->NextNode != NULL)
+        {
+            Tail = Tail->NextNode;
+        }
+
+        Tail->NextNode = NewNode;
+        NewNode->PrevNode = Tail;
+    }
+}
+
+// DLL 노드 탐색
+Node* DLL_GetNode(Node* Head, int Location)
+{
+    Node* Current = Head;
+
+    while (Current != NULL && (--Location) >= 0)
+    {
+        Current = Current->NextNode;
+    }
+
+    return Current;
+}
+
+// DLL 노드 삽입
+void DLL_InsertAfter(Node* Current, Node* NewNode)
+{
+    NewNode->NextNode = Current->NextNode;
+    NewNode->PrevNode = Current;
+
+    if (Current->NextNode != NULL)
+    {
+        Current->NextNode->PrevNode = NewNode;
+        Current->NextNode = NewNode;
+    }
+}
+
+// DLL 노드 제거
+void DLL_RemoveNode(Node** Head, Node* Remove)
+{
+    if (*Head == Remove)
+    {
+        *Head = Remove->NextNode;
+        if ((*Head) != NULL)
+            (*Head)->PrevNode = NULL;
+
+        Remove->PrevNode = NULL;
+        Remove->NextNode = NULL;
+    }
+    else
+    {
+        Node* Temp = Remove;
+
+        if (Remove->PrevNode != NULL)
+            Remove->PrevNode->NextNode = Temp->NextNode;
+
+        if (Remove->NextNode != NULL)
+            Remove->NextNode->PrevNode = Temp->PrevNode;
+
+        Remove->PrevNode = NULL;
+        Remove->NextNode = NULL;
+    }
+}
+
+// DLL 노드 개수
+int DLL_GetNodeCount(Node* Head)
+{
+    unsigned int  Count = 0;
+    Node* Current = Head;
+
+    while (Current != NULL)
+    {
+        Current = Current->NextNode;
+        Count++;
+    }
+
+    return Count;
+}

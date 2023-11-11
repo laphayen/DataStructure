4c406ad491aa36999fb2798635ab8795994812a3
Baek Gi Chan <laphayen@gmail.com>
2023-11-12 03:36:53 +0900

    Update ArrayStack

--- LinearStructure/stack/ArrayStack/ArrayStack.c
+++ LinearStructure/stack/ArrayStack/ArrayStack.c
@@ -3,10 +3,13 @@
 // AS 생성
 void AS_CreateStack(ArrayStack** Stack, int Capacity)
 {
+	// 스택을 자유 저장소에 할당
 	(*Stack) = (ArrayStack*)malloc(sizeof(ArrayStack));
 	
+	// 스택 용량만큼 노드를 자유 저장소에 할당
 	(*Stack)->Nodes = (Node*)malloc(sizeof(Node) *Capacity);
 
+	// 스택 용량과 Top 초기화
 	(*Stack)->Capacity = Capacity;
 	(*Stack)->Top = 0;
 }
@@ -14,12 +17,14 @@ void AS_CreateStack(ArrayStack** Stack, int Capacity)
 // AS 소멸
 void AS_DestroyStack(ArrayStack* Stack)
 {
+	// 배열 스택 노드를 자유 저장소에서 해제
 	free(Stack->Nodes);
 
+	// 배열 스택을 자유 저장소에서 해제
 	free(Stack);
 }
 
-// AS Push
+// AS 데이터 삽입
 void AS_Push(ArrayStack* Stack, ElementType Data)
 {
 	int Position = Stack->Top;
@@ -28,7 +33,7 @@ void AS_Push(ArrayStack* Stack, ElementType Data)
 	Stack->Top++;
 }
 
-// AS Pop
+// AS 데이터 제거
 ElementType AS_Pop(ArrayStack* Stack)
 {
 	int Position = --(Stack->Top);
@@ -36,19 +41,27 @@ ElementType AS_Pop(ArrayStack* Stack)
 	return Stack->Nodes[Position].Data;
 }
 
-// AS Top
+// AS 맨 위 데이터 반환
 ElementType AS_Top(ArrayStack* Stack)
 {
 	int Position = Stack->Top - 1;
 
+	if (AS_IsEmpty(Stack))
+	{
+		printf("Stack is Empty\n");
+		exit(-1);
+	}
+	
 	return Stack->Nodes[Position].Data;
 }
 
+// AS 크기 반환
 int AS_GetSize(ArrayStack* Stack)
 {
 	return Stack->Top;
 }
 
+// AS 비어있는지 여부
 int AS_IsEmpty(ArrayStack* Stack)
 {
 	return (Stack->Top == 0);
--- LinearStructure/stack/ArrayStack/ArrayStack.h
+++ LinearStructure/stack/ArrayStack/ArrayStack.h
@@ -7,25 +7,26 @@
 typedef int ElementType;
 
 // AS 노드
-typedef struct tagNode
+typedef struct Node
 {
 	ElementType Data;
 } Node;
 
-// AS 구조체는 Stack의 용량, 현재 위치, 노드들을 가리키는 포인터를 가진다.
-typedef struct tagArrayStack
+// AS 구조체
+typedef struct ArrayStack
 {
 	int Capacity;
 	int Top;
 	Node* Nodes;
 } ArrayStack;
 
-void        AS_CreateStack(ArrayStack** Stack, int Capacity);
-void        AS_DestroyStack(ArrayStack* Stack);
-void        AS_Push(ArrayStack* Stack, ElementType Data);
+// AS 연산
+void AS_CreateStack(ArrayStack** Stack, int Capacity);
+void AS_DestroyStack(ArrayStack* Stack);
+void AS_Push(ArrayStack* Stack, ElementType Data);
 ElementType AS_Pop(ArrayStack* Stack);
 ElementType AS_Top(ArrayStack* Stack);
-int         AS_GetSize(ArrayStack* Stack);
-int         AS_IsEmpty(ArrayStack* Stack);
+int AS_GetSize(ArrayStack* Stack);
+int AS_IsEmpty(ArrayStack* Stack);
 
 #endif

4706e295fad688f416493f9839c3b3f7d03cdbe3
Baek Gi Chan <laphayen@gmail.com>
2023-11-14 17:20:15 +0900

    Update CircularQueue

--- LinearStructure/queue/CircularQueue/CircularQueue.c
+++ LinearStructure/queue/CircularQueue/CircularQueue.c
@@ -28,7 +28,14 @@ void CQ_Enqueue(CircularQueue* Queue, ElementType Data)
 {
     int Position = 0;
 
-    // Rear가 Capacity에 도달하면 배열의 인덱스 처음
+    // 큐가 가득찬 경우
+    if (CQ_IsFull(Queue))
+    {
+        printf("Queue is Full\n");
+        return;
+    }
+    
+    // Rear가 Capacity에 도달하면 배열의 인덱스 처음으로 변경
     if (Queue->Rear == Queue->Capacity)
     {
         Position = Queue->Rear;
@@ -36,41 +43,72 @@ void CQ_Enqueue(CircularQueue* Queue, ElementType Data)
     }
     // Rear가 Capacity에 도달하지 않았다면 Rear를 증가
     else
+    {
         Position = Queue->Rear++;
+    }
     
+    // Rear에 데이터 삽입
     Queue->Nodes[Position].Data = Data;
 }
 
+// CQ 데이터 삭제
 ElementType CQ_Dequeue(CircularQueue* Queue)
 {
     int Position = Queue->Front;
 
-    if (Queue->Front == Queue->Capacity)
-        Queue->Front = 0;
-    else
-        Queue->Front++;
+    // 큐가 비어있는 경우
+    if (CQ_IsEmpty(Queue))
+    {
+        printf("Queue is Empty\n");
+        return 0;
+    }
 
+    // Front가 Capacity에 도달하면 배열의 인덱스 처음으로 변경
+    if (Queue->Front == Queue->Capacity)
+    {
+        Queue->Front = 0;
+    }
+    // Front가 Capacity에 도달하지 않았다면 Front를 증가
+    else
+    {
+        Queue->Front++;
+    }
+
+    // Front에 있는 데이터 반환
     return Queue->Nodes[Position].Data;
 }
 
+// CQ 크기 반환
 int CQ_GetSize(CircularQueue* Queue)
 {
     if(Queue->Front <= Queue->Rear)
+    {
         return Queue->Rear - Queue->Front;
-    // 리어가 앞으로 오면 처리하는 방법
+    }
+    // 배열에서 Rear가 Front보다 앞에 있는 경우
     else
-        return Queue->Front + (Queue->Capacity - Queue->Front) + 1;
+    {
+        return Queue->Capacity - (Queue->Front - Queue->Rear) + 1;
+    }
 }
 
+// CQ 비여있는지 확인
 int CQ_IsEmpty(CircularQueue* Queue)
 {
     return (Queue->Front == Queue->Rear);
 }
 
+// CQ 가득차있는지 확인
 int CQ_IsFull(CircularQueue* Queue)
 {
+    // 배열에서 Rear가 Front보다 뒤에 있는 경우
     if (Queue->Front < Queue->Rear)
+    {
         return (Queue->Rear - Queue->Front) == Queue->Capacity;
+    }
+    // 배열에서 Rear가 Front보다 앞에 있는 경우
     else
+    {
         return (Queue->Rear + 1) == Queue->Front;
+    }
 }

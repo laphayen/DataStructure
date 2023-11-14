a9f9e7a448c57712dd69ffd1bf5d9e31303a63de
Baek Gi Chan <laphayen@gmail.com>
2023-11-14 15:05:58 +0900

    Add comment to CircularQueue

--- LinearStructure/queue/CircularQueue/CircularQueue.c
+++ LinearStructure/queue/CircularQueue/CircularQueue.c
@@ -1,31 +1,40 @@
 #include "CircularQueue.h"
 
+// CQ 생성
 void CQ_CreateQueue(CircularQueue** Queue, int Capacity)
 {
+    // 원형 큐를 자유 저장소에 할당
     (*Queue) = (CircularQueue*)malloc(sizeof(CircularQueue));
-
+     
+    // 노드 배열을 자유 저장소에 할당 - 더미 노드 추가
     (*Queue)->Nodes = (Node*)malloc(sizeof(Node)* (Capacity+1));
 
+    // 원형 큐 초기화
     (*Queue)->Capacity = Capacity;
     (*Queue)->Front = 0;
     (*Queue)->Rear = 0;
 }
 
+// CQ 소멸
 void CQ_DestroyQueue(CircularQueue* Queue)
 {
+    // 노드 배열을 자유 저장소에서 해제
     free(Queue->Nodes);
     free(Queue);
 }
 
+// CQ 데이터 삽입
 void CQ_Enqueue(CircularQueue* Queue, ElementType Data)
 {
     int Position = 0;
 
+    // Rear가 Capacity에 도달하면 배열의 인덱스 처음
     if (Queue->Rear == Queue->Capacity)
     {
         Position = Queue->Rear;
         Queue->Rear = 0;
     }
+    // Rear가 Capacity에 도달하지 않았다면 Rear를 증가
     else
         Position = Queue->Rear++;
     
@@ -48,7 +57,7 @@ int CQ_GetSize(CircularQueue* Queue)
 {
     if(Queue->Front <= Queue->Rear)
         return Queue->Rear - Queue->Front;
-        // 리어가 앞으로 오면 처리하는 방법
+    // 리어가 앞으로 오면 처리하는 방법
     else
         return Queue->Front + (Queue->Capacity - Queue->Front) + 1;
 }
--- LinearStructure/queue/CircularQueue/CircularQueue.h
+++ LinearStructure/queue/CircularQueue/CircularQueue.h
@@ -6,20 +6,22 @@
 
 typedef int ElementType;
 
-typedef struct tagNode
+// 노드 구조체
+typedef struct Node
 {
     ElementType Data;
 } Node;
 
-typedef struct tagCircularQueue
+// 원형 큐 구조체
+typedef struct CircularQueue
 {
-    int Capacity;
-    int Front;
-    int Rear;
-
-    Node* Nodes;
+    int Capacity;   // 큐의 용량
+    int Front;      // 큐의 전단 
+    int Rear;       // 큐의 후단
+    Node* Nodes;    // 노드 배열
 } CircularQueue;
 
+// 원형 큐 함수
 void CQ_CreateQueue(CircularQueue** Queue, int Capacity);
 void CQ_DestroyQueue(CircularQueue* Queue);
 void CQ_Enqueue(CircularQueue* Queue, ElementType Data);
@@ -28,4 +30,4 @@ int CQ_GetSize(CircularQueue* Queue);
 int CQ_IsEmpty(CircularQueue* Queue);
 int CQ_IsFull(CircularQueue* Queue);
 
-#endif
+#endif // CIRCULAR_QUEUE_H

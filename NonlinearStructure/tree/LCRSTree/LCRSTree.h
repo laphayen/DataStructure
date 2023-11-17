13cd803f02ade90ab59c50226029ee35445d592a
Baek Gi Chan <laphayen@gmail.com>
2023-11-17 17:36:40 +0900

    Update LCRSTree

--- NonlinearStructure/tree/LCRSTree/LCRSTree.h
+++ NonlinearStructure/tree/LCRSTree/LCRSTree.h
@@ -4,21 +4,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// 트리 데이터 타입 정의
 typedef char ElementType;
 
-typedef struct tagLCRSNode
+// 트리 노드 구조체
+typedef struct LCRSNode
 {
-	struct tagLCRSNode* LeftChild;
-	struct tagLCRSNode* RightSibling;
+	// 왼쪽 자식 노드를 가리키는 포인터
+	struct LCRSNode* LeftChild;
+	// 오른쪽 형제 노드를 가리키는 포인터
+	struct LCRSNode* RightSibling;
 
+	// 노드의 데이터를 저장하는 멤버
 	ElementType Data;
 } LCRSNode;
 
 LCRSNode* LCRS_CreateNode(ElementType NewData);
 void LCRS_DestroyNode(LCRSNode* Node);
 void LCRS_DestroyTree(LCRSNode* Node);
-
 void LCRS_AddChildNode(LCRSNode* ParentNode, LCRSNode* ChildNode);
 void LCRS_PrintTree(LCRSNode* Node, int Depth);
 
-#endif
+#endif // !LCRS_TREE_H
